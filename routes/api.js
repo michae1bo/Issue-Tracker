ccad85b8552d09ce183034e1bc88a34331410b6a
michae1bo <159921466+michae1bo@users.noreply.github.com>
2025-01-27 18:07:36 +0000

    feat: groud issues by project

--- routes/api.js
+++ routes/api.js
@@ -8,14 +8,17 @@ module.exports =  function (app) {
   mongoose.connect(process.env.MONGO_URI);
 
   const issueTrackerSchema = new mongoose.Schema({
-    issue_title: String,
-    issue_text: String,
-    created_on: Date,
-    updated_on: Date,
-    created_by: String,
-    assigned_to: String,
-    open: Boolean,
-    status_text: String
+    project: String,
+    issues: [{
+        issue_title: String,
+        issue_text: String,
+        created_on: Date,
+        updated_on: Date,
+        created_by: String,
+        assigned_to: String,
+        open: Boolean,
+        status_text: String
+      }]
   });
 
   const IssueTracker = mongoose.model('IssueTracker', issueTrackerSchema);
@@ -29,6 +32,7 @@ module.exports =  function (app) {
       let project = req.params.project;
       let body = req.body;
       let response;
+      let projectResponse = await IssueTracker.findOne({ project: project });
       if (body.issue_title && body.issue_text && body.created_by) {
         const issueObject = {};
         issueObject.issue_title = body.issue_title;
@@ -39,12 +43,18 @@ module.exports =  function (app) {
         issueObject.assigned_to = body.assigned_to ? body.assigned_to : "";
         issueObject.status_text = body.status_text ? body.status_text : "";
         issueObject.open = true;
-        const newIssue = new IssueTracker(issueObject);
-        response = await newIssue.save();
+        if (projectResponse === null) {
+          const newProject = new IssueTracker({ project: project, issues: [issueObject] });
+          const savedProject = await newProject.save();
+          response = savedProject.issues.pop();
+        } else {
+          projectResponse.issues.push(issueObject);
+          const updatedProject = await projectResponse.save();
+          response = updatedProject.issues.pop();
+        }
       } else {
         response =  {error: "required field(s) missing" };
       }
-      console.log(response);
       res.json(response);
     })
     
