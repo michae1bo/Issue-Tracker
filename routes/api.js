ff600c9568cc33fb5bc6f8fb2229741c3a763082
michae1bo <159921466+michae1bo@users.noreply.github.com>
2025-01-27 18:35:22 +0000

    feat: implement get api

--- routes/api.js
+++ routes/api.js
@@ -24,8 +24,67 @@ module.exports =  function (app) {
   const IssueTracker = mongoose.model('IssueTracker', issueTrackerSchema);
   app.route('/api/issues/:project')
   
-    .get(function (req, res){
+    .get(async function (req, res){
       let project = req.params.project;
+      let response;
+      const projectResponse = await IssueTracker.findOne({ project: project });
+      if (projectResponse === null) {
+        response = { error: 'no such project'};
+      } else {
+        let issues = projectResponse.issues;
+        if (req.query._id) {
+          issues = issues.filter((issue) => {
+            return issue._id == req.query._id;
+          });
+        }
+        if (req.query.issue_title) {
+          issues = issues.filter((issue) => {
+            return issue.issue_title == req.query.issue_title;
+          });
+        }
+        if (req.query.issue_text) {
+          issues = issues.filter((issue) => {
+            return issue.issue_text == req.query.issue_text;
+          });
+        }
+        if (req.query.created_on) {
+          issues = issues.filter((issue) => {
+            return issue.created_on == req.query.created_on;
+          });
+        }
+        if (req.query.updated_on) {
+          issues = issues.filter((issue) => {
+            return issue.updated_on == req.query.updated_on;
+          });
+        }
+        if (req.query.created_by) {
+          issues = issues.filter((issue) => {
+            return issue.created_by === req.query.created_by;
+          });
+          console.log(issues);
+        }
+        if (req.query.assigned_to) {
+          issues = issues.filter((issue) => {
+            return issue.assigned_to == req.query.assigned_to;
+          });
+        }
+        if (req.query.status_text) {
+          issues = issues.filter((issue) => {
+            return issue.status_text == req.query.status_text;
+          });
+        }
+        if (req.query.open === "true") {
+          issues = issues.filter((issue) => {
+            return issue.open === true;
+          });
+        } else if(req.query.open === "false") {
+          issues = issues.filter((issue) => {
+            return issue.open === false;
+          });
+        }
+        response = issues;
+      }
+      res.json(response);
     })
     
     .post(async function (req, res){
